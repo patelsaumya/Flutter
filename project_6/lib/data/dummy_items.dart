60e0428e6a4e09748f59db982fbc64b1a38defeb
patelsaumya <saumyapatel.sap@gmail.com>
2025-09-07 15:35:22 +0530

    feat: Project 6

---
+++ project_6/lib/data/dummy_items.dart
@@ -0,0 +1,24 @@
+import 'package:project_6/models/category.dart';
+import 'package:project_6/models/grocery_item.dart';
+import 'package:project_6/data/categories.dart';
+
+final groceryItems = [
+  GroceryItem(
+      id: 'a',
+      name: 'Milk',
+      quantity: 1,
+      category: categories[Categories.dairy]!
+  ),
+  GroceryItem(
+      id: 'b',
+      name: 'Bananas',
+      quantity: 5,
+      category: categories[Categories.fruit]!
+  ),
+  GroceryItem(
+      id: 'c',
+      name: 'Beef Steak',
+      quantity: 1,
+      category: categories[Categories.meat]!
+  )
+];
